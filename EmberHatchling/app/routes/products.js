e19eadcd1184e25d8677c5540710c8813fe3b414
Chris A <ca033@hdm-stuttgart.de>
2016-11-23 12:41:17 +0100

    Back to DummyDaten

--- EmberHatchling/app/routes/products.js
+++ EmberHatchling/app/routes/products.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model(){
-    return this.get('store').query('product', {});
+		return [
+			{id:'1', name: 'Grünes Kleid', description: 'Grünes Kleid mit blauer Jacke', price: '15 Euro', url: '../../../public/assets/images/products/Kleidung_01.png'},
+			{id:'2', name: 'Grünes Kleid', description: 'Grünes Kleid mit blauer Jacke', price: '15 Euro', url: '../../../public/assets/images/products/Kleidung_02.png'},
+			{id:'3', name: 'Grünes Kleid', description: 'Grünes Kleid mit blauer Jacke', price: '15 Euro', url: '../../../public/assets/images/products/Kleidung_03.png'},
+			{id:'4', name: 'Grünes Kleid', description: 'Grünes Kleid mit blauer Jacke', price: '15 Euro', url: '../../../public/assets/images/products/Kleidung_04.jpg'},
+			{id:'5', name: 'Grünes Kleid', description: 'Grünes Kleid mit blauer Jacke', price: '15 Euro', url: '../../../public/assets/images/products/Kleidung_05.jpg'},
+		];
+    // return this.get('store').query('product', {});
 	}
 });
