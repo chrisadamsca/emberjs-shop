85b73dc9ac3776401f8cbb60e9c597b5af8cd670
Darja Ferber <df029@hdm-stuttgart.de>
2016-11-27 13:07:08 +0100

    Working with params.product_id for individual route

--- EmberHatchling/app/routes/product.js
+++ EmberHatchling/app/routes/product.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(){
-  	return this.get('store').findRecord('product', '58354d77e4670512dcfd5a41');
+  model(params){
+  	return this.get('store').findRecord('product', params.product_id);
   }
 });
