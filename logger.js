b02f08828b9f88ee6ca38980daa7aa3a925dc30a
nils kristjansson <nk078@hdm-stuttgart.de>
2016-12-14 14:32:56 +0100

    added logger:

---
+++ logger.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+var logger = 
+{
+    log : "",
+    logFile : "logfile.log",
+    logErr : function logErr( err)
+    {
+
+        var stack = new Error(err).stack.split("\n");
+
+        this.appendToLog( this.logDate() + " | ");
+        this.appendToLog( stack[0]+"\n");
+
+        for(var i = 2; i < stack.length && !stack[i].includes("at Module._compile");i++)
+        {            
+            this.appendToLog( stack[i]+"\n");
+        }
+    },
+    logWarn : function logErr( warn)
+    {
+        this.appendToLog( this.logDate() + " | Warning: ");
+               
+        this.appendToLog( warn+"\n");
+    },
+    logDebug : function logErr( debug)
+    {
+        this.appendToLog( this.logDate() + " | Debug: ");
+               
+        this.appendToLog(  debug+"\n");
+    },
+    logDate : function()
+    {
+         return new Date().toUTCString();
+    },
+    appendToLog : function(append)
+    {
+        console.log(append.substring(0,append.indexOf("\n")));
+        if(this.logFile != "")
+        {
+            fs.appendFileSync(this.logFile,append,function(err){});
+        }
+    }
+
+
+}
+
+module.exports = logger;
