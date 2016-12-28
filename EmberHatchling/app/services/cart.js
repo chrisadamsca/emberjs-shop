586620e23dfe803d8892f7b54f76eccaf4f2bc47
Darja Ferber <df029@hdm-stuttgart.de>
2016-12-28 15:16:15 +0100

    Sum up all the stuff in cart

--- EmberHatchling/app/services/cart.js
+++ EmberHatchling/app/services/cart.js
@@ -2,18 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: null,
-  sum: 0,
+  prices: Ember.computed.mapBy('items', 'price'),
+  sum: Ember.computed.sum('prices'),
 
   init() {
     this._super(...arguments);
     this.set('items', []);
-    this.set('sum', 0);
-    this.get('items')["sum"] = 0;
   },
 
   add(newItem) {
-    this.get('items')["sum"]+=newItem.price;
-
     for(var i=0; i<this.get('items').length; i++){
       if(this.get('items')[i].id === newItem.id){
         this.get('items')[i].quantity++;
