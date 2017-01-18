77514bb20843f30ad9a975191917739b1eb1163d
Chris A <ca033@hdm-stuttgart.de>
2017-01-18 15:35:54 +0100

    Startseite zeigt Produkte an

--- EmberHatchling/app/router.js
+++ EmberHatchling/app/router.js
@@ -8,7 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login');
-  this.route('products');
+  this.route('products', {path: '/'});
   this.route('product', {path: 'products/:product_id'});
   this.route('cart');
   this.route('boys');
