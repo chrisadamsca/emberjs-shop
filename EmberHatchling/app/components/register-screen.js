cbc5313814d8ac2a1af324716e0f996a70478a32
Chris A <ca033@hdm-stuttgart.de>
2017-01-18 12:30:50 +0100

    Registration mit direktem Login funktioniert.

--- EmberHatchling/app/components/register-screen.js
+++ EmberHatchling/app/components/register-screen.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  session: Ember.inject.service('session'),
+
+  actions: {
+    register() {
+      var that = this;
+      if(this.get("password") !== this.get("re-password")){
+        return;
+      }
+      var formData = {
+          "name":this.get("name"),
+          "surname":this.get("surname"),
+          "email":this.get("email"),
+          "password":this.get("password"),
+          "address":this.get("address")
+      }
+
+      $.post("/api/user", {
+        user: formData
+      }).then(function() {
+        that.get('session').authenticate('authenticator:oauth2', formData.email, formData.password).catch((reason) => {
+          that.set('errorMessage', reason.error || reason);
+        });
+      }, function() {
+        this.set("loginFailed", true);
+      }.bind(this));
+    }
+  }
 });
