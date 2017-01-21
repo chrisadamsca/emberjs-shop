bf1ff3af452dca1d351d30cd9b30625a3f4939ba
nils kristjansson <nk078@hdm-stuttgart.de>
2017-01-21 16:36:20 +0100

    if not logged in order ridirects one to login

--- EmberHatchling/app/components/cart-order.js
+++ EmberHatchling/app/components/cart-order.js
@@ -4,6 +4,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
+    session: Ember.inject.service('session'),
   actions: {
     order(items) {
       var order = {
